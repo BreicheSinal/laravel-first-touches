44ba705961a569d05bcdf5d6b817d2968b598b54
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 14:16:40 +0200

    Merged users table with admins

--- database/migrations/2024_11_23_000946_users.php
+++ database/migrations/2024_11_23_000946_users.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
         });
     }
