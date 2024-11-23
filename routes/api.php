ecd6d7172dbe96b4f288bf63b559baee2a1b7257
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 17:03:36 +0200

    Added all admin routes

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// admin routes
+Route::prefix('admin/news')->group(function(){
+    Route::post("/{admin_id}", [AdminController::class, "post_news"]);
+    Route::put("/{admin_id}/{id}", [AdminController::class, "edit_news"]);
+    Route::delete("/{admin_id}/{id}", [AdminController::class, "delete_news"]);
+});
