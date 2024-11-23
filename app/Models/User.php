9e6f8c45f2f70f501b71c277e9063ecd6f94c6fa
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 14:45:32 +0200

    Added passowrd to a hidden field

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,13 +11,4 @@ class User extends Model {
         "password",
         "role"
     ];
-
-    public function isUser(): bool{
-        return $this->role === 'user';
-    }
-
-    public function isAdmin(): bool{
-        return $this->role === 'admin';
-    }
-
 }
