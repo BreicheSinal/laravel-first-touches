44623a4111078216656ccf7c90ac6f7831d1434b
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 17:10:30 +0200

    Added all user routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
 // admin routes
 Route::prefix('admin/news')->group(function(){
     Route::post("/{admin_id}", [AdminController::class, "post_news"]);
     Route::put("/{admin_id}/{id}", [AdminController::class, "edit_news"]);
     Route::delete("/{admin_id}/{id}", [AdminController::class, "delete_news"]);
+});
+
+// users routes
+Route::prefix('users/news')->group(function(){
+    Route::get("/", [UserController::class, "get_news"]);
+    Route::post("/{user_id}/{news_id}", [UserController::class, "post_article"]);
 });
