88171dc3e90bdada2752de9ea3f1cef8efb6d415
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 13:39:22 +0200

    Completed articles_news schema

--- database/migrations/2024_11_23_002104_articles_linked_news.php
+++ database/migrations/2024_11_23_002104_articles_linked_news.php
@@ -6,19 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        //
+    public function up(): void{
+        Schema::create('articles_linked_news', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('news_id')->constrained('news')->onDelete('cascade');
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        //
+    public function down(): void{
+        Schema::dropIfExists('articles_linked_news');
     }
 };
