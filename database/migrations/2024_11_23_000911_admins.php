e96eebefb461f9ec54a41a85110b9b38bbb2d2bf
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 13:35:07 +0200

    Completed admins schema

--- database/migrations/2024_11_23_000911_admins.php
+++ database/migrations/2024_11_23_000911_admins.php
@@ -6,19 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        //
+    public function up(): void{
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('admins');
     }
 };
