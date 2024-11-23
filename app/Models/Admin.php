d4a5ca95e8d7c8166dd59d0ce322f162458310ce
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 13:51:10 +0200

    Created news model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
-{
+class Admin extends Model{
     protected $fillable = [
         "username", 
         "email", 
