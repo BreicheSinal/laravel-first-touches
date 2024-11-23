902046f79dad55865d502830de30b9862dc0b796
BreicheSinal <sinalbreiche@gmail.com>
2024-11-23 14:53:57 +0200

    Created method to get news for user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    function get_news(){
+        $news = News::all();
+
+        return response()->json([
+            "news" => $news
+        ]);
+    }
 }
