21c787953afc0154e927f312be2da3b726b01e91
BreicheSinal <sinalbreiche@gmail.com>
2024-11-25 16:10:26 +0200

    Implemented JWT authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model {
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
     protected $fillable = [
         "username", 
         "email", 
